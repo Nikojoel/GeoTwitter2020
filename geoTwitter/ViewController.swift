2523f518ed41bda2d8ce8878b194e286d0524d42
Enar Mariinsky <enar.mariinsky@gmail.com>
2020-04-07 10:42:14 +0300

    Initial commit

---
+++ geoTwitter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  geoTwitter
+//
+//  Created by iosdev on 7.4.2020.
+//  Copyright © 2020 enarm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
