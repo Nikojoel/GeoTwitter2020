24697f7541330440b999c5d5f5e3d3af93a3456d
Enar Mariinsky <enar.mariinsky@gmail.com>
2020-04-14 10:13:34 +0300

    Hard coded keys

---
+++ geoTwitter/Api/HardCodedGitignore.swift
@@ -0,0 +1,16 @@
+//
+//  HardCodedGitignore.swift
+//  geoTwitter
+//
+//  Created by iosdev on 14.4.2020.
+//  Copyright © 2020 enarm. All rights reserved.
+//
+
+import Foundation
+
+enum HardCodedKeys: String {
+    case consumerKey = "bssuRW5T1gJT9YLfKZbQlIQsy"
+    case consumerSecret = "YpRElHXdAzLLqVaFYzyumJ9b7pwZ2VI7ggN9LPQeJrGMfaenBV"
+    case accessToken = "2288566045-JtWcletGEnqSQHaQFr65w6XQm5l7WTSzbaB4Ik3"
+    case tokenSecret = "AQD5lXjNtM9buJoijQMp4fDU8jb2lSP6vJafO3Gy7J5X9"
+}
