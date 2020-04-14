5064d2ddf4c65353850b4acf9212626024425c31
Enar Mariinsky <enar.mariinsky@gmail.com>
2020-04-14 18:17:23 +0300

    commeneted out fetching on master branch

--- geoTwitter/Api/APIfetch.swift
+++ geoTwitter/Api/APIfetch.swift
@@ -12,32 +12,32 @@ import OAuthSwift
 class APIFetch {
     private var oauth: OAuth1Swift?
     
-    func fetchAPI(query: String?) {
-        
-        let urlEncoded = query?.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
-        
-        oauth = OAuth1Swift (
-            consumerKey:    HardCodedKeys.consumerKey.rawValue,
-            consumerSecret: HardCodedKeys.consumerSecret.rawValue,
-            requestTokenUrl: "https://api.twitter.com/oauth/request_token",
-            authorizeUrl:    "https://api.twitter.com/oauth/authorize",
-            accessTokenUrl:  "https://api.twitter.com/oauth/access_token"
-        )
-        oauth?.client.credential.oauthToken = HardCodedKeys.userToken.rawValue
-        oauth?.client.credential.oauthTokenSecret = HardCodedKeys.userSecret.rawValue
-        
-        oauth?.client.get("https://api.twitter.com/1.1/search/tweets.json?q=\(urlEncoded ?? "")") { result in
-            switch result {
-            case .success(let response):
-                let dataString = response.string
-                print(dataString ?? "no data string")
-                
-                // TODO decoding to json data
-            case .failure(let error):
-                print(error)
-            }
-        }
-    }
+//    func fetchAPI(query: String?) {
+//        
+//        let urlEncoded = query?.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
+//        
+//        oauth = OAuth1Swift (
+//            consumerKey:    HardCodedKeys.consumerKey.rawValue,
+//            consumerSecret: HardCodedKeys.consumerSecret.rawValue,
+//            requestTokenUrl: "https://api.twitter.com/oauth/request_token",
+//            authorizeUrl:    "https://api.twitter.com/oauth/authorize",
+//            accessTokenUrl:  "https://api.twitter.com/oauth/access_token"
+//        )
+//        oauth?.client.credential.oauthToken = HardCodedKeys.userToken.rawValue
+//        oauth?.client.credential.oauthTokenSecret = HardCodedKeys.userSecret.rawValue
+//        
+//        oauth?.client.get("https://api.twitter.com/1.1/search/tweets.json?q=\(urlEncoded ?? "")") { result in
+//            switch result {
+//            case .success(let response):
+//                let dataString = response.string
+//                print(dataString ?? "no data string")
+//                
+//                // TODO decoding to json data
+//            case .failure(let error):
+//                print(error)
+//            }
+//        }
+//    }
     
     
 }
