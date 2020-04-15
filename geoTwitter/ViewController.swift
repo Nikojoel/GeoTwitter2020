82bca2ece48f1603dbd17aebcb9259288fd045b1
Enar Mariinsky <enar.mariinsky@gmail.com>
2020-04-15 10:50:08 +0300

    commiting

--- geoTwitter/ViewController.swift
+++ geoTwitter/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("starting")
-        APIFetch().fetchAPI(query: "donald trump")
+        //APIFetch().fetchAPI(query: "donald trump")
     }
 
 
