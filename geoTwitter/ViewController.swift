3382473744395f26c89949016f677f9a5e2e1d60
Enar Mariinsky <enar.mariinsky@gmail.com>
2020-04-15 13:47:05 +0300

    rxswit testing with fetch

--- geoTwitter/ViewController.swift
+++ geoTwitter/ViewController.swift
@@ -7,15 +7,34 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
-
+    
+    var tweets: [TweetQuery] = [] {
+        didSet {
+            for tweet in tweets {
+                print(tweet.tweet.count)
+            }
+            
+        }
+    }
+    let api = APIFetch()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        APIFetch().fetchAPI(query: "#helsinki")
+        
+        // rxswift subscription example
+        //        api.subject
+        //            .subscribe {
+        //                if let element = $0.element {
+        //                    self.tweets.append(element)
+        //                }
+        //        }
+        //        api.fetchAPI(query: "#helsinki")
+        //    }
+        
+        
     }
-
-
 }
 
