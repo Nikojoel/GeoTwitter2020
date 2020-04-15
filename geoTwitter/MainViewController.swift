ed8ea0eef239da1a1f3cb888af275ce9e1540ce5
Enar Mariinsky <enar.mariinsky@gmail.com>
2020-04-15 22:25:20 +0300

    login logic and presenting tableview

---
+++ geoTwitter/MainViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  geoTwitter
+//
+//  Created by iosdev on 7.4.2020.
+//  Copyright © 2020 enarm. All rights reserved.
+//
+import Foundation
+import UIKit
+import RxSwift
+
+class MainViewController: UIViewController {
+    
+    private let userDefaults = UserDefaults.standard
+    var auth = Auth()
+    @IBOutlet weak var logInButton: UIButton!
+    
+    var tweets: [TweetQuery] = [] {
+        didSet {
+            for tweet in tweets {
+                print(tweet.tweet.count)
+            }
+            
+        }
+    }
+    let api = APIFetch()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+        // rxswift subscription example
+        //        api.subject
+        //            .subscribe {
+        //                if let element = $0.element {
+        //                    self.tweets.append(element)
+        //                }
+        //        }
+        //        api.fetchAPI(query: "#helsinki")
+        //    }
+        
+        //auth.authUserToken()
+        
+        
+        //var auth = Auth()
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        checkTokenAndPresent()
+    }
+    
+    @IBAction func pressLogInButton(_ sender: UIButton) {
+        auth.authUserToken()
+        _ = auth.status
+            .subscribe {
+                if $0.element ?? false {
+                    self.logInButton.isHidden = true
+                    self.checkTokenAndPresent()
+                }
+        }
+    }
+}
+
+
+// Custom functions extension
+extension MainViewController {
+    
+    func checkTokenAndPresent() {
+       if (userDefaults.string(forKey: "userToken") != nil && userDefaults.string(forKey: "userSecret") != nil) {
+            let vc = tabTableViewController()
+            vc.modalPresentationStyle = .fullScreen
+            present(vc, animated: true)
+        }
+    }
+}
