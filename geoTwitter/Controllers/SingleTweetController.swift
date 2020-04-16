4662837b7ec2d57b0dd298fb265b74ff1520f5d2
Jalmari Espo <jalmares@metropolia.fi>
2020-04-16 16:59:01 +0300

    First version of the single view

--- geoTwitter/Controllers/SingleTweetController.swift
+++ geoTwitter/Controllers/SingleTweetController.swift
@@ -12,10 +12,42 @@ class SingleTweetController: UIViewController {
     
     var tweet: Tweet?
 
+    let profPic = UIImage(named: "profPic1")
+    
+    
     @IBOutlet weak var testiLabel: UILabel!
+    @IBOutlet weak var tweetTextLabel: UILabel!
+    @IBOutlet weak var profilePicture: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var followersLabel: UILabel!
+    @IBOutlet weak var friendsLabel: UILabel!
+    @IBOutlet weak var tweetFavoriteLabel: UILabel!
+    @IBOutlet weak var retweetLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         testiLabel.text = tweet?.user.name
+        tweetTextLabel.text = tweet?.text
+        tweetTextLabel.sizeToFit()
+        profilePicture.image = profPic
+        dateLabel.text = tweet?.created_at
+        followersLabel.text = "\(tweet?.user.followers_count ?? 0)"
+        friendsLabel.text = "\(tweet?.user.friends_count ?? 0)"
+        tweetFavoriteLabel.text = "\(tweet?.favorite_count ?? 0)"
+        retweetLabel.text = "\(tweet?.retweet_count ?? 0)"
+       /* date formatting, not working yet
+        let input = tweet?.created_at
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM d, h:mm a"
+        if let date = formatter.date(from: input) {
+            print(date)
+            
+            let displayFormatter = DateFormatter()
+                displayFormatter.dateFormat = "MMM d, h:mm a"
+        dateLabel.text = displayFormatter.string(from: date)
+        }
+         */
+    }
+            
         // Do any additional setup after loading the view.
     }
     
@@ -30,4 +62,3 @@ class SingleTweetController: UIViewController {
     }
     */
 
-}
