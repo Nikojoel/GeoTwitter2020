d65c50f674eff8bc1496b6da6ace9297edfed8ac
Enar Mariinsky <enar.mariinsky@gmail.com>
2020-04-19 21:34:15 +0300

    EndPoint

--- geoTwitter/Api/EndPoint.swift
+++ geoTwitter/Api/EndPoint.swift
@@ -6,4 +6,17 @@
 //  Copyright © 2020 enarm. All rights reserved.
 //
 
-import Foundation
+enum EndPoint: String {
+    case url = "https://api.twitter.com/1.1/"
+    case account = "account/verify_credentials.json"
+    case query = "search/tweets.json"
+    case newMessage = "direct_messages/events/new.json"
+    case listDirectMessages = "direct_messages/events/list.json"
+    case showDirectMessage = "direct_messages/events/show.json"
+    case getClosestWOEID = "trends/closest.json"
+    case getTrends = "trends/place.json"
+    case newTweet = "statuses/update.json"
+    case deleteTweet = "statuses/destroy/" //:id.json
+    case likeTweet = "favorites/create.json"
+    case deleteLike = "favorites/destroy.json"
+}
